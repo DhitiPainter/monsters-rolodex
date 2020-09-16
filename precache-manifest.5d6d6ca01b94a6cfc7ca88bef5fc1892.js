5d31661430b81143af0fd5cec686b0c8b9b6ca9c
Dhiti Painter <dhiti.painter@litera.com>
2020-09-16 17:39:49 +0530

    Updates

---
+++ precache-manifest.5d6d6ca01b94a6cfc7ca88bef5fc1892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fd94a7d172e8a595c2ef6a594b8fdf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "987ccbc5cfec57706411",
+    "url": "/monsters-rolodex/static/css/main.fa22f813.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987ccbc5cfec57706411",
+    "url": "/monsters-rolodex/static/js/main.a00ab0f9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
